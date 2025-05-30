9ca6285c072249dfadae3325a8a71c2b02567e63
Alexis Mclaren mclaren370@gmail.com <mclaren370@gmail.com>
2025-05-30 11:42:00 +0100

    learning path page

--- app/controllers/learning_paths_controller.rb
+++ app/controllers/learning_paths_controller.rb
@@ -16,10 +16,9 @@ class LearningPathsController < ApplicationController
       redirect_to root_path, alert: "Failed to create learning path."
     end
   end
-  
+
   def show
-    #recommited
-    @learning_path = current_user.learning_paths.find(params[:id])
-    @milestones = Milestone.all
+    @learning_path = LearningPath.find(params[:id])
+    @milestones = @learning_path.milestones
   end
 end
