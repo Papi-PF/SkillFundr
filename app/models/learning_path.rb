44170b6061b53c2e6fe577b569bf4cd16b1cf63a
Paul Foma Ndieye <paulfoma6@gmail.com>
2025-06-02 18:22:37 +0100

    update

--- app/models/learning_path.rb
+++ app/models/learning_path.rb
@@ -10,13 +10,4 @@ class LearningPath < ApplicationRecord
   validates :user, presence: true
   validates :skill, presence: true
   validates :skill_id, uniqueness: { scope: :user_id }
-
-  def content
-    client = OpenAI::Client.new
-    chatgpt_response = client.chat(parameters: {
-        model:"gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Give me a the step by step learning path of how to learn Javascript." } ]
-      })
-    return chatgpt_response["choices"][0]["message"]["content"]
-  end
 end
