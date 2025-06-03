6e40fe027cb650f238fb2246a9cb6fbad6b0da00
Paul Foma Ndieye <paulfoma6@gmail.com>
2025-06-03 15:02:06 +0100

    new refresh

--- app/controllers/learning_paths_controller.rb
+++ app/controllers/learning_paths_controller.rb
@@ -33,6 +33,12 @@ class LearningPathsController < ApplicationController
     # @milestones = @learning_path.milestones
   end
 
+  def create_from_skill
+    skill_id = params[:skill_id]
+    current_user.learning_paths.create(skill_id: skill_id)
+    redirect_to dashboard_path, notice: "Continuing your learning on this skill"
+  end
+
   def destroy
     @learning_path = current_user.learning_paths.find(params[:id])
     @learning_path.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :skills, only: [:index, :show]
   resources :learning_paths, only: [:create, :show, :destroy]
   resources :milestones, only: [:show, :update]
+  post '/dashboard', to: 'learning_paths#create_from_skill', as: :create_learning_path_from_skill
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
